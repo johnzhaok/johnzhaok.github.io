9b81be3db1e762cb7c0c1b7c576da10d516ee330
John Zhao <johnzhaok@gmail.com>
2025-08-08 08:55:54 -0400

    Name switch update

---
+++ assets/js/scripts.js
@@ -0,0 +1,18 @@
+/**
+ *  Switch between English and Chinese name
+ */
+var switchNames = () => {
+    var englishName = document.getElementById("nameSwapEnglish");
+    var chineseName = document.getElementById("nameSwapChinese");
+
+    englishName.classList.toggle("hidden");
+    chineseName.classList.toggle("hidden");
+}
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    var colorBlockButton = document.getElementById("colorBlockButton");
+
+    if (colorBlockButton !== null) {
+        colorBlockButton.addEventListener("click", switchNames);
+    }
+});
